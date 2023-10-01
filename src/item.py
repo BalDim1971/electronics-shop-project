0cf515368fb1154e139a59b29ff64dcaa6955647
BalashovDV <korochun@yandex.ru>
2023-10-01 16:43:30 +0300

    Изменения: 1. атрибут 'name' приватный 2. добавлены сеттер и геттер для 'name' через @property 3. сеттер 'name' проверяет длину наименования и обрезает до 10 символов при необходимости 4. добавлен классовый метод `instantiate_from_csv()` получения данных из файла формата csv. В рамках задания для чтения файла происходит переход на уровень выше 5. добавлен статический метод `string_to_number()` возвращающий целое число из строки

--- src/item.py
+++ src/item.py
@@ -1,10 +1,11 @@
+import csv
+
 class Item:
     """
     Класс для представления товара в магазине.
     """
     pay_rate = 1.0
     all = []
-
     
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
@@ -14,11 +15,10 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
-
     
     def calculate_total_price(self) -> float:
         """
@@ -27,10 +27,52 @@ class Item:
         :return: Общая стоимость товара.
         """
         return self.price * self.quantity
-
     
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+    
+    # Геттер для name
+    @property
+    def name(self):
+        '''
+        Возвращает значение приватной переменной __name
+        '''
+        return self.__name
+    
+    # Сеттер для __name
+    @name.setter
+    def name(self, name):
+        '''
+        Установить значение __name, если длина слова больше 10 - взять первые 10
+        '''
+        if len(name) > 10:
+            name = name[0:10]
+        self.__name = name
+    
+    @classmethod
+    def instantiate_from_csv(cls, name_file):
+        '''
+        Функция инициации класса данными из файла
+        Реализовано в рамках конкретного задания: сделан переход для чтения файла от уровня выше
+        '''
+        
+        cls.all.clear()
+        with open('../'+name_file, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                item = cls(row['name'], float(row['price']), int(row['quantity']))
+    
+    @staticmethod
+    def string_to_number(num: str):
+        '''
+        Превращаем строку-число в целое число
+        Если число с точкой, отрезаем лишнее после точки
+        '''
+        
+        pos_point = num.find('.')
+        if pos_point != -1:
+            num = num[0:pos_point]
+        return int(num)
--- tests/test_item.py
+++ tests/test_item.py
@@ -11,14 +11,40 @@ item2 = Item("Ноутбук", 20000, 5)
 def test_calculate_total_price():
 	'''
 	Когда создан класс Смартфон, то должно вернуть 200000
+	Для Ноутбук - 100000
 	'''
 	assert item1.calculate_total_price() == 200000
 	assert item2.calculate_total_price() == 100000
 
 
 def test_apply_discount():
+	'''
+	Тестируем применение дискаунта
+	'''
 	Item.pay_rate = 0.8
 	item1.apply_discount()
 	assert item1.price == 8000.0
 	assert item2.price == 20000
 	
+
+def test_name():
+	'''
+	Тестирование возвращение значения приватной переменной
+	'''
+	assert item1.name == "Смартфон"
+	assert item2.name == "Ноутбук"
+
+
+def test_name_set():
+	'''
+	Тестируем функцию задания имени
+	'''
+	item1.name = 'Калькулятор'
+	assert item1.name == 'Калькулято'
+
+
+def test_string_to_number():
+	'''
+	Тестируем преобразование из строки-числа в целое число
+	'''
+	assert Item.string_to_number('5.5') == 5
